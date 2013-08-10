0d7561e46d5c1ea7b41b916d02fed7e05bd6c934
Jason Weathered <jason@jasoncodes.com>
2013-08-10 22:33:45 +1000

    Don't try to create views while running `rake db:migrate`
    Running migrations loads the app but the DB probably isn't ready yet for
    views with dependencies. Wait to create views until the main app load.

--- lib/active_record_views/extension.rb
+++ lib/active_record_views/extension.rb
@@ -2,6 +2,12 @@ module ActiveRecordViews
   module Extension
     extend ActiveSupport::Concern
 
+    def self.currently_migrating?
+      if defined? Rake
+        Rake.application.top_level_tasks.include?('db:migrate')
+      end
+    end
+
     module ClassMethods
       def is_view(sql = nil)
         sql ||= begin
@@ -9,7 +15,9 @@ module ActiveRecordViews
           ActiveRecordViews.register_for_reload self, sql_path
           File.read sql_path
         end
-        ActiveRecordViews.create_view self.connection, self.table_name, sql
+        unless ActiveRecordViews::Extension.currently_migrating?
+          ActiveRecordViews.create_view self.connection, self.table_name, sql
+        end
       end
     end
   end
