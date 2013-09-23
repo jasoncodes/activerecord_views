0694641945d102af145567d5f77abc9f110ee444
Jason Weathered <jason@jasoncodes.com>
2013-09-23 19:48:41 +1000

    Revert "Use an ActiveRecord model to manage checksum cache"
    Unfortunately, it seems difficult to attach ActiveRecord::Base objects
    to different connection objects. This will be needed to implement a
    feature shortly which preserves views created within transactions.
    
    This reverts commit 3e22f9853ce82205a01d6dd485789ddf5458998a.
    
    Conflicts:
    	lib/active_record_views/checksum_cache.rb
    	spec/active_record_views_extension_spec.rb

--- lib/active_record_views/checksum_cache.rb
+++ lib/active_record_views/checksum_cache.rb
@@ -1,12 +1,5 @@
-require 'active_record'
-
 module ActiveRecordViews
   class ChecksumCache
-    class Model < ActiveRecord::Base
-      self.table_name = 'active_record_views'
-      self.primary_key = 'name'
-    end
-
     def initialize(connection)
       @connection = connection
       init_state_table!
@@ -19,16 +12,16 @@ module ActiveRecordViews
     end
 
     def get(name)
-      Model.where(:name => name).first_or_initialize.checksum
+      @connection.select_value("SELECT checksum FROM active_record_views WHERE name = #{@connection.quote name}")
     end
 
     def set(name, checksum)
-      row = Model.where(:name => name).first_or_initialize
       if checksum
-        row.checksum = checksum
-        row.save!
+        if @connection.update("UPDATE active_record_views SET checksum = #{@connection.quote checksum} WHERE name = #{@connection.quote name}") == 0
+          @connection.insert "INSERT INTO active_record_views (name, checksum) VALUES (#{@connection.quote name}, #{@connection.quote checksum})"
+        end
       else
-        row.destroy
+        @connection.delete "DELETE FROM active_record_views WHERE name = #{@connection.quote name}"
       end
     end
   end
