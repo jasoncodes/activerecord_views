67a8322d68e7574052e82b20c31498e4a683d55f
Jason Weathered <jason@jasoncodes.com>
2015-03-25 08:37:29 +1000

    Add test coverage for structure dumping

---
+++ spec/tasks_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'rake db:structure:dump' do
+  it 'copies over activerecord_views data' do
+    ActiveRecordViews.create_view ActiveRecord::Base.connection, 'test_view', 'TestView', 'SELECT 1'
+
+    FileUtils.rm_f 'spec/internal/db/structure.sql'
+    system("rake -f spec/internal/Rakefile db:structure:dump")
+    raise unless $?.success?
+
+    sql = File.read('spec/internal/db/structure.sql')
+    FileUtils.rm_f 'spec/internal/db/structure.sql'
+
+    expect(sql).to match /COPY active_record_views.+test_view\tTestView/m
+  end
+end
