90f1aeea5435ac9d36a95afd0c89917f98a0bcd6
Jason Weathered <jason@jasoncodes.com>
2015-05-22 16:36:33 +1000

    Fix warning

--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -11,6 +11,6 @@ describe 'rake db:structure:dump' do
     sql = File.read('spec/internal/db/structure.sql')
     FileUtils.rm_f 'spec/internal/db/structure.sql'
 
-    expect(sql).to match /COPY active_record_views.+test_view\tTestView/m
+    expect(sql).to match(/COPY active_record_views.+test_view\tTestView/m)
   end
 end
