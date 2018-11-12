428cae1373fd26169aa64be4ccd359e69fc0c979
Jason Weathered <jason@jasoncodes.com>
2018-11-13 08:15:12 +1000

    Wrap examples in a group for the spec file

--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 
-describe 'rake db:structure:dump' do
-  it 'copies over activerecord_views data' do
-    ActiveRecordViews.create_view ActiveRecord::Base.connection, 'test_view', 'TestView', 'SELECT 1'
+describe 'rake tasks' do
+  describe 'db:structure:dump' do
+    it 'copies over activerecord_views data' do
+      ActiveRecordViews.create_view ActiveRecord::Base.connection, 'test_view', 'TestView', 'SELECT 1'
 
-    FileUtils.rm_f 'spec/internal/db/structure.sql'
-    system("rake -f spec/internal/Rakefile db:structure:dump")
-    raise unless $?.success?
+      FileUtils.rm_f 'spec/internal/db/structure.sql'
+      system("rake -f spec/internal/Rakefile db:structure:dump")
+      raise unless $?.success?
 
-    sql = File.read('spec/internal/db/structure.sql')
-    FileUtils.rm_f 'spec/internal/db/structure.sql'
+      sql = File.read('spec/internal/db/structure.sql')
+      FileUtils.rm_f 'spec/internal/db/structure.sql'
 
-    expect(sql).to match(/COPY public\.active_record_views.+test_view\tTestView/m)
-    expect(sql).to match(/UPDATE public\.active_record_views SET refreshed_at = NULL/)
+      expect(sql).to match(/COPY public\.active_record_views.+test_view\tTestView/m)
+      expect(sql).to match(/UPDATE public\.active_record_views SET refreshed_at = NULL/)
+    end
   end
 end
