dbb8c39f6dedf39422afbdbd0f9e35c6d7ce69ba
Jason Weathered <jason@jasoncodes.com>
2021-04-24 14:27:47 +1000

    Add support for RSpec :focus and :skip

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,8 @@ require 'rspec/rails'
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
+  config.filter_run_when_matching focus: true
+  config.filter_run_excluding skip: true
   config.example_status_persistence_file_path = 'tmp/examples.txt'
 
   config.before do
