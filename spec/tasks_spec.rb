7fd489f2b28aaae13f2fad4d7d444862003de6dc
Jason Weathered <jason@jasoncodes.com>
2021-04-28 09:14:25 +1000

    Extract schema task setup and teardown out of the main example

--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -52,15 +52,20 @@ describe 'rake tasks' do
   end
 
   describe 'db:structure:dump' do
-    it 'copies over activerecord_views data' do
-      ActiveRecordViews.create_view ActiveRecord::Base.connection, 'test_view', 'TestView', 'SELECT 1'
-
+    before do
       FileUtils.rm_f 'spec/internal/db/structure.sql'
+
+      ActiveRecordViews.create_view ActiveRecord::Base.connection, 'test_view', 'TestView', 'SELECT 1'
+    end
+
+    after do
+      FileUtils.rm_f 'spec/internal/db/structure.sql'
+    end
+
+    it 'copies over activerecord_views data' do
       rake 'db:structure:dump'
 
       sql = File.read('spec/internal/db/structure.sql')
-      FileUtils.rm_f 'spec/internal/db/structure.sql'
-
       expect(sql).to match(/COPY public\.active_record_views.+test_view\tTestView/m)
       expect(sql).to match(/UPDATE public\.active_record_views SET refreshed_at = NULL/)
     end
