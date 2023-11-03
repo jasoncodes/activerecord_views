ba7f1d6e965ed680751601933013c4a846fd0cdb
Jason Weathered <jason@jasoncodes.com>
2023-11-03 13:22:56 +1000

    Ignore internal warning from ActiveSupport 7.0.x

--- spec/support/silence_warnings.rb
+++ spec/support/silence_warnings.rb
@@ -1,6 +1,12 @@
 require 'warning'
 require 'rails/version'
 
+case Rails::VERSION::STRING
+when /^7\.0\./
+  Warning.ignore(%r{lib/active_support/core_ext/time/deprecated_conversions.rb:\d+: warning: method redefined; discarding old to_s})
+  Warning.ignore(%r{lib/active_support/time_with_zone.rb:\d+: warning: previous definition of to_s was here})
+end
+
 Warning.process do |_warning|
   :raise
 end
